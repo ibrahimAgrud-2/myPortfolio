fa695afc7a5a7f22da7223d6b6c455fc60087a97
ibrahim <ibrahimagrud7@gmail.com>
2026-06-15 09:13:24 +0300

    appSettings.json added

---
+++ Data/appDbContext .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Models;
+
+namespace Portfolio.Data
+{
+
+    //DBContext ile DB ile bağlantı kurup DB'yi yönetebiliriz. Tıpkı ADO.NET'te olan sqlconnetion gibi. appDbContext Dbcontext sınıfını miras alarak DB'yi yönetebilir hale geliyor. 
+    public class appDbContext: DbContext
+    {
+
+        //bu class sadece about me sınıfını  bağlı olacak about me tablosunu yöneteceğini DbSet<aboutMe> ile sölyemiş oluyoruz. EF buna göre tablo oluşturacak. 
+
+        //about me sınıfnına bakarar DB'de bir tablo oluşacak.
+        public DbSet<aboutMe> aboutMe { get; set; }
+
+       public  appDbContext(DbContextOptions<appDbContext> options):base(options)
+        { 
+
+
+        }
+    }
+}
---
+++ Models/aboutMe.cs
@@ -0,0 +1,24 @@
+﻿namespace Portfolio.Models
+{
+    public class aboutMe
+    {
+
+
+        //EF'nin property'leri görebilmesi için public olmalı
+        public int ID { set; get; }
+      public string greeting { set; get; }
+      public  string fistName { set; get; }
+      public string lastName { set; get; }
+     
+      public string description { set; get; }
+     
+
+
+
+
+
+
+
+    }
+
+}
